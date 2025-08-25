31933a04448c3264be0bc9a9e96122cebeb4da08
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-08-25 23:29:42 +0900

    feat:다크테마

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movie_market/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
@@ -10,11 +11,16 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('home'),
+      title: '무비 마켓',
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          useMaterial3: true,
+          scaffoldBackgroundColor: const Color(0xFF0E0E0F)),
+      home: HomePage(),
     );
   }
 }

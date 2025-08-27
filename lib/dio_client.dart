1030a9ede633467de9685d3509e042144190cfc8
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-08-27 10:17:40 +0900

    feat:DIO생성

---
+++ lib/dio_client.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+//통신
+class DioClient {
+// await dotenv.load(fileName: ".env");
+  static Dio get client => _client;
+
+  static Dio _client = Dio(
+    BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3/movie/',
+      validateStatus: (status) => true,
+    ),
+  )..interceptors.add(interceptor); //하이잭킹 -> 헤더
+  static AuthInterceptor interceptor = AuthInterceptor();
+}
+
+class AuthInterceptor extends Interceptor {
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) {
+    //
+    options.headers.addAll(
+      {
+        'Authorization': 'Bearer ${dotenv.env['TMDB_TOKEN']}',
+        'accept': 'application/json',
+      },
+    );
+  }
+}

3c37cea14f87ed521245d62ea8b5c7b4eb269548
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-08-27 10:18:36 +0900

    feat: 레파지토리 생성

---
+++ lib/data/repository/movie_repository_impl.dart
@@ -0,0 +1,106 @@
+import 'package:flutter_movie_market/data/datasource/movie_data_source.dart';
+import 'package:flutter_movie_market/domain/entity/movie.dart';
+import 'package:flutter_movie_market/domain/entity/movie_detail.dart';
+import 'package:flutter_movie_market/domain/repository/movie_repository.dart';
+
+class MovieRepositoryImpl implements MovieRepository {
+  final MovieDataSource movieDataSource;
+
+  MovieRepositoryImpl(this.movieDataSource);
+
+  @override
+  Future<MovieDetail?> fetchMovieDetail(int id) async {
+    final source = await movieDataSource.fetchMovieDetail(id);
+    if (source == null) return null;
+
+    return MovieDetail(
+      budget: source.budget,
+      genres: List.from(
+        source.genres.map((e) => e.name),
+      ),
+      id: source.id,
+      productionCompanyLogos: List.from(
+        source.productionCompanies.map(
+          (e) => e.logoPath,
+        ),
+      ),
+      overview: source.overview,
+      popularity: source.popularity,
+      releaseDate: source.releaseDate,
+      revenue: source.revenue,
+      runtime: source.runtime,
+      tagline: source.tagline,
+      title: source.title,
+      voteAverage: source.voteAverage,
+      voteCount: source.voteCount,
+    );
+  }
+
+  @override
+  Future<List<Movie>?> fetchNowPlayingMovies() async {
+    final source = await movieDataSource.fetchNowPlayingMovies();
+    if (source == null) return null;
+
+    return List<Movie>.from(
+      source.results.map(
+        (e) {
+          return Movie(
+            id: e.id,
+            posterPath: e.posterPath,
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  Future<List<Movie>?> fetchPopularMovies() async {
+    final source = await movieDataSource.fetchPopularMovies();
+    if (source == null) return null;
+
+    return List<Movie>.from(
+      source.results.map(
+        (e) {
+          return Movie(
+            id: e.id,
+            posterPath: e.posterPath,
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  Future<List<Movie>?> fetchTopRatedMovies() async {
+    final source = await movieDataSource.fetchTopRatedMovies();
+    if (source == null) return null;
+
+    return List<Movie>.from(
+      source.results.map(
+        (e) {
+          return Movie(
+            id: e.id,
+            posterPath: e.posterPath,
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  Future<List<Movie>?> fetchUpcomingMovies() async {
+    final source = await movieDataSource.fetchUpcomingMovies();
+    if (source == null) return null;
+
+    return List<Movie>.from(
+      source.results.map(
+        (e) {
+          return Movie(
+            id: e.id,
+            posterPath: e.posterPath,
+          );
+        },
+      ),
+    );
+  }
+}
